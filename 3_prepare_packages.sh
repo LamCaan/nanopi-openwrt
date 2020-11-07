9e842891aab55db2693709aba686306c181c9a44
CaanLAM <43984893+CaanLAM@users.noreply.github.com>
2020-11-07 14:05:44 +0800

    packages following lede

--- 3_prepare_packages.sh
+++ 3_prepare_packages.sh
@@ -1,5 +1,6 @@
 ./scripts/feeds update -a && ./scripts/feeds install -a
-rm -rf feeds/packages/libs/libcap && svn co https://github.com/openwrt/packages/trunk/libs/libcap feeds/packages/libs/libcap
+rm -rf feeds/packages/libs/libcap/ && svn co https://github.com/openwrt/packages/trunk/libs/libcap feeds/packages/libs/libcap
+rm -rf feeds/packages/lang/golang/ && svn co https://github.com/coolsnowwolf/packages/trunk/lang/golang feeds/packages/lang/golang
 sed -i '/enable-jsonc/i\\t--disable-cloud \\' feeds/packages/admin/netdata/Makefile 
 
 cd package/lean/
@@ -11,6 +12,9 @@ rm -rf luci-theme-argon*/ ../lean/luci-theme-argon/ && git clone -b 18.06 https:
 rm -rf luci-app-chinadns-ng/ && git clone https://github.com/WuSiYu/luci-app-chinadns-ng
 rm -rf openwrt-chinadns-ng/ && git clone https://github.com/pexcn/openwrt-chinadns-ng
 rm -rf luci-app-smartdns/ && git clone -b lede https://github.com/pymumu/luci-app-smartdns.git
+rm -rf v2ray/ && svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/v2ray
+rm -rf v2ray-plugin/ && svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/v2ray-plugin
+rm -rf trojan/ && svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/trojan
 cd ../../
 
 #git clone https://github.com/jerrykuku/lua-maxminddb.git
