c2022d351a75ebca25b1559f2df3a44fb64a1a72
CaanLAM <43984893+CaanLAM@users.noreply.github.com>
2021-02-02 19:50:05 +0800

    Update 3_prepare_packages.sh

--- 3_prepare_packages.sh
+++ 3_prepare_packages.sh
@@ -8,15 +8,18 @@ sed -i 's,"eth1" "eth0","eth0" "eth1",g' target/linux/rockchip-rk3328/base-files
 sed -i "s,'eth1' 'eth0','eth0' 'eth1',g" target/linux/rockchip-rk3328/base-files/etc/board.d/02_network
 
 cd package/lean/
-if [[ `pwd` == *"rk3328"* ]]; then
-  rm -rf openwrt-smartdns/ && git clone https://github.com/pymumu/openwrt-smartdns
-fi
-[ ! -d luci-app-adguardhome ] && git clone https://github.com/rufengsuixing/luci-app-adguardhome
-rm -rf luci-theme-argon*/ ../lean/luci-theme-argon/ && git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon
+#if [[ `pwd` == *"rk3328"* ]]; then
+#  rm -rf openwrt-smartdns/ && git clone https://github.com/pymumu/openwrt-smartdns
+#fi
+#[ ! -d luci-app-adguardhome ] && git clone https://github.com/rufengsuixing/luci-app-adguardhome
+rm -rf luci-app-adguardhome/ && git clone --depth 1 https://github.com/rufengsuixing/luci-app-adguardhome
+#rm -rf luci-theme-argon*/ ../lean/luci-theme-argon/ && git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon
+rm -rf luci-theme-argon*/ ../lean/luci-theme-argon/ && git clone -b master --depth 1 https://github.com/jerrykuku/luci-theme-argon
 #rm -rf luci-app-chinadns-ng/ && git clone https://github.com/WuSiYu/luci-app-chinadns-ng
-rm -rf luci-app-chinadns-ng/ && git clone -b luci https://github.com/pexcn/openwrt-chinadns-ng.git luci-app-chinadns-ng
-rm -rf openwrt-chinadns-ng/ && git clone https://github.com/pexcn/openwrt-chinadns-ng
-rm -rf luci-app-smartdns/ && git clone -b lede https://github.com/pymumu/luci-app-smartdns.git
+rm -rf luci-app-chinadns-ng/ && git clone -b luci --depth 1 https://github.com/pexcn/openwrt-chinadns-ng.git luci-app-chinadns-ng
+rm -rf openwrt-chinadns-ng/ && git clone --depth 1 https://github.com/pexcn/openwrt-chinadns-ng
+rm -rf openwrt-smartdns/ && git clone --depth 1 https://github.com/pymumu/openwrt-smartdns
+rm -rf luci-app-smartdns/ && git clone -b lede --depth 1 https://github.com/pymumu/luci-app-smartdns.git
 #rm -rf v2ray/ && svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/v2ray
 #rm -rf v2ray-plugin/ && svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/v2ray-plugin
 rm -rf trojan/ && svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/trojan
@@ -26,9 +29,10 @@ svn co https://github.com/songchenwen/nanopi-r2s/trunk/luci-app-r2sflasher
 #svn co https://github.com/openwrt/openwrt/branches/openwrt-19.07/package/network/utils/iputils package/network/utils/iputil
 
 #流量监视
-git clone -b master --depth 1 https://github.com/brvphoenix/wrtbwmon.git package/new/wrtbwmon
-git clone -b master --depth 1 https://github.com/brvphoenix/luci-app-wrtbwmon.git package/new/luci-app-wrtbwmon
-
+#git clone -b master --depth 1 https://github.com/brvphoenix/wrtbwmon.git package/new/wrtbwmon
+#git clone -b master --depth 1 https://github.com/brvphoenix/luci-app-wrtbwmon.git package/new/luci-app-wrtbwmon
+git clone -b master --depth 1 https://github.com/brvphoenix/wrtbwmon.git
+git clone -b master --depth 1 https://github.com/brvphoenix/luci-app-wrtbwmon.git
 cd ../../
 
 #git clone https://github.com/jerrykuku/lua-maxminddb.git
