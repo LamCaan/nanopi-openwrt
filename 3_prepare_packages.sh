350b5fdd9a1e34a0a5c578c3f22444a23bca74b1
CaanLAM <43984893+CaanLAM@users.noreply.github.com>
2021-02-03 15:18:06 +0800

    Update 3_prepare_packages.sh

--- 3_prepare_packages.sh
+++ 3_prepare_packages.sh
@@ -27,12 +27,12 @@ svn co https://github.com/songchenwen/nanopi-r2s/trunk/luci-app-r2sflasher
 #serverchan
 #git clone -b master --depth 1 https://github.com/tty228/luci-app-serverchan.git package/new/luci-app-serverchan
 #svn co https://github.com/openwrt/openwrt/branches/openwrt-19.07/package/network/utils/iputils package/network/utils/iputil
-
-#流量监视
-git clone -b master --depth 1 https://github.com/brvphoenix/wrtbwmon.git ../new/wrtbwmon
-#git clone -b master --depth 1 https://github.com/brvphoenix/luci-app-wrtbwmon.git ../new/luci-app-wrtbwmon
-rm -rf luci-app-wrtbwmon/ && git clone -b master --depth 1 https://github.com/brvphoenix/luci-app-wrtbwmon.git
 cd ../../
+#流量监视
+git clone -b master --depth 1 https://github.com/brvphoenix/wrtbwmon.git package/new/wrtbwmon
+git clone -b master --depth 1 https://github.com/brvphoenix/luci-app-wrtbwmon.git package/new/luci-app-wrtbwmon
+#rm -rf luci-app-wrtbwmon/ && git clone -b master --depth 1 https://github.com/brvphoenix/luci-app-wrtbwmon.git
+
 
 #git clone https://github.com/jerrykuku/lua-maxminddb.git
 #git clone https://github.com/jerrykuku/luci-app-vssr.git
