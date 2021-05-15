8779098d2eb1c59fd636867139a3ca3d20344bba
CaanLAM <43984893+CaanLAM@users.noreply.github.com>
2021-05-15 18:22:59 +0800

    Update merge_packages.sh

--- merge_packages.sh
+++ merge_packages.sh
@@ -14,9 +14,9 @@ function merge_package(){
     mv $pn package/
 }
 
-merge_package https://github.com/linkease/ddnsto-openwrt
-merge_package https://github.com/project-lede/luci-app-godproxy
+#merge_package https://github.com/linkease/ddnsto-openwrt
+#merge_package https://github.com/project-lede/luci-app-godproxy
 merge_package https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-cifsd luci-app-ksmbd
 merge_package https://github.com/coolsnowwolf/packages/trunk/kernel/ksmbd
 merge_package https://github.com/coolsnowwolf/packages/trunk/net/ksmbd-tools
-merge_package https://github.com/Beginner-Go/luci-app-tencentddns
+#merge_package https://github.com/Beginner-Go/luci-app-tencentddns
